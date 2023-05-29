a2aa8d8c97633e3ca7d5480c0c769a46469d3fcc
Johnlouie Gutierrez <johgu.829@gmail.com>
2023-05-29 11:59:43 -0700

    New commits

---
+++ CreatingANewClass.cs
@@ -0,0 +1,21 @@
+﻿class CreatingANewClass {
+  public static void Main(string[] args) {
+    Student s1 = new Student();
+    s1.Name = "Bob";
+    s1.Age = 15;
+    s1.FinalGrade = 65;
+    s1.SayHello();
+
+    Student s2 = new Student();
+    s2.Name = "Frank";
+    s2.Age = 12;
+    s2.FinalGrade = 90;
+    s2.SayHello();
+
+    Student s3 = new Student();
+    s3.Name = "Ted";
+    s3.Age = 15;
+    s3.FinalGrade = 95;
+    s3.SayHello();
+  }
+}
---
+++ Student.cs
@@ -0,0 +1,9 @@
+class Student {
+  public string Name;
+  public int Age;
+  public double FinalGrade;
+
+  public void SayHello() {
+    Console.WriteLine("Hello from {0}", Name);
+  }
+}
